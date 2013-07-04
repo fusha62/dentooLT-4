257408f6c5c07cee8eabbf7688e56c849256a201
Tomohiro Minagawa <fusha01@hotmail.com>
2013-07-05 02:17:51 +0900

    Windowに分解して，色々処理するための関数（書きかけ）をコミット．

---
+++ function_sep_window.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% Window Separation for signal vect %%%%%
+%%%%%                   for Dentoo LT 4 %%%%%
+%%%%%            written by T.Minagawa  %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function out_mat = function_sep_window(data,win_len)
+  out_mat = zeros(length,floor(length(data)/win_len)*2-1);
+  for i = 1:floor(length(data)/win_len)*2-1
+    
+  end;
+
+end
