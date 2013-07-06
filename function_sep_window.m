e85e7b0810fb9fcd387ba94711817da1b4024d13
Tomohiro Minagawa <fusha01@hotmail.com>
2013-07-06 21:34:25 +0900

    分割窓は矩形を利用したほうが便利だということに気づいた． 合成時はハニング窓を利用している．

--- function_sep_window.m
+++ function_sep_window.m
@@ -6,12 +6,10 @@
 
 function out_mat = function_sep_window(data,win_len)
   out_mat = zeros(win_len,floor(length(data)/win_len)*2-1);
-  windows = hann(win_len);
   for i = 1:floor(length(data)/win_len)*2-1
     out_mat(:,i) = ...
 	data(floor(win_len/2)*(i-1)+1:...
-	     floor(win_len/2)*(i-1)+win_len)...
-	.*windows;
+	     floor(win_len/2)*(i-1)+win_len);
   end;
 
 end
