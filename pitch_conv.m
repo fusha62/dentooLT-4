9b5076f80a88d0b1356b8b256a32310974978c3f
Tomohiro Minagawa <fusha01@hotmail.com>
2013-07-07 01:05:24 +0900

    ピッチ変換を行うスクリプトをコミット．

---
+++ pitch_conv.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%    Function of Pitch convert      %%%%%
+%%%%%                   for Dentoo LT 4 %%%%%
+%%%%%            written by T.Minagawa  %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Setting
+% pitch rate
+p_rate = 1.8;
+% window length
+w_len = 512;
+
+
+%% files input
+[data, fs] = wavread('./infile.wav');
+
+
+%% Pitch convert!!
+out_mat = function_sep_window(data(:,1),w_len);
+pout_mat = function_pitch_conv(out_mat,p_rate);
+out_vec_l = function_merge_window(pout_mat);
+out_mat = function_sep_window(data(:,2),w_len);
+pout_mat = function_pitch_conv(out_mat,p_rate);
+out_vec_r = function_merge_window(pout_mat);
+out_vec = horzcat(out_vec_l,out_vec_r);
+
+%% files output
+wavwrite(out_vec,fs,'./test.wav');
